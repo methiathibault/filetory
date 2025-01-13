32eb64d126f5e803a0c3c311b2a0c5ae7d283c10
dundun <dcostes@aveco.fr>
2025-01-13 19:28:56 +0100

    ajout list name pour userpage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import {React ,useState} from 'react'
+import axios from 'axios'
 
 import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
     let navigate = useNavigate();
 
+      const [newMail, setNewMail] = useState("");
+      const [newPassword, setNewPassword] = useState("");
+
     const go_subscribe =() => {
         navigate("/subscribe")
     }
     const go_login = () => {
-        console.log('you are loged in successfully')
-        navigate("/files")
+        axios.post('http://127.0.0.1:3001/users/connection', {
+            email: newMail,
+            password: newPassword
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
 
 
@@ -19,11 +31,11 @@ export default function LoginPage() {
             <div className='font-bold text-2xl'>LoginPage</div>
             <div>
                 email :
-                <input className='rounded-md border-2 border-black px-2'></input>
+                <input onChange={e => setNewMail(e.target.value)} className='rounded-md border-2 border-black px-2'></input>
             </div>
             <div>
                 password :
-                <input className='rounded-md border-2 border-black px-2'></input>
+                <input onChange={e => setNewPassword(e.target.value)} className='rounded-md border-2 border-black px-2'></input>
             </div>
             <div className=''>
                 <button onClick={go_login} className='rounded-xl border-2 border-black px-4'>login</button>
--- frontend/src/pages/UsersPageTest.jsx
+++ frontend/src/pages/UsersPageTest.jsx
@@ -1,14 +1,41 @@
-import React, { Component } from 'react'
+import {React ,useState, useEffect} from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
-export class UsersPageTest extends Component {
+export default function UsersPageTest() {
+    const [usersList, setUsersList] = useState([]);
 
 
-    
-  render() {
+
+    const getUsers = () => {
+        console.log("get users");
+        axios.get("http://127.0.0.1:3001/users/")
+            .then(function(response){
+                console.log(response.data)
+                setUsersList(response.data)
+            })
+    }
+
+    useEffect(()=>{
+        getUsers()
+    },[]);
+
+
     return (
-      <div>UsersPageTest</div>
+        <div>
+            <div>UsersPageTest</div>
+            <div>
+                <h1>LISTE </h1>
+                {usersList.map(result =>
+                   
+                    <div key={result.id}>
+                        name : {result.email}
+                    </div>
+                   
+                )}
+            </div>
+        </div>
+      
     )
-  }
+  
 }
-
-export default UsersPageTest
