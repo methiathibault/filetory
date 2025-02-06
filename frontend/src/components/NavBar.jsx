5f90af3a1b232251fe407a038e65d5f3f2ec293b
methiathibault <methiathibault@gmail.com>
2025-02-06 13:05:26 +0100

    Refactor NavBar component to improve admin check and streamline button rendering

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,81 +1,64 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { useUserContext } from '../context/AuthContext';
 import { jwtDecode } from "jwt-decode";
-import axios from 'axios'
+import axios from 'axios';
+
+const NavButton = ({ to, children }) => (
+  <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150'>
+    <Link to={to}>{children}</Link>
+  </div>
+);
+
 export default function NavBar() {
-  const { token, verifyToken, checkToken } = useUserContext();
-  const [isAdmin, setAdmin] = useState(false)
+  const { token, verifyToken } = useUserContext();
+  const [isAdmin, setAdmin] = useState(false);
 
-  const checkAdmin = () => {
-    
-        const decodedToken = jwtDecode(token);
-        
-
-        axios.get("http://127.0.0.1:3001/users/admin/"+decodedToken.userId,
-            {
-                headers:{
-                    "authorization":token
-                }
-            },
-          
-        )
-        .then(response => {setAdmin(response.data.admin)})
-        .catch(error => {
-            console.error("Erreur lors de la vérification admin :", error);
-            return false; // ou throw error si besoin
-        });
+  useEffect(() => {
+    if (token) {
+      checkAdmin();
     }
+  }, [token]);
+
+  const checkAdmin = async () => {
+    try {
+      const decodedToken = jwtDecode(token);
+      const response = await axios.get(
+        `http://127.0.0.1:3001/users/admin/${decodedToken.userId}`,
+        {
+          headers: {
+            "authorization": token
+          }
+        }
+      );
+      setAdmin(response.data.admin);
+    } catch (error) {
+      console.error("Erreur lors de la vérification admin :", error);
+      setAdmin(false);
+    }
+  };
 
   return (
-    <div className='flex gap-2 justify-between rounded-full border-2 m-4 p-2 bg-zinc-100'>
-        <div className='flex gap-2 mr-4 items-center '>
-        <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150  '>  <Link to="/">Home</Link> </div>
-          
-           
-            
-        </div>
-        <div className='flex gap-2 mr-4 items-center'>
-        {verifyToken() ? "":
+    <nav className='flex gap-2 justify-between rounded-full border-2 m-4 p-2 bg-zinc-100'>
+      <div className='flex gap-2 mr-4 items-center'>
+        <NavButton to="/">Home</NavButton>
+      </div>
+      <div className='flex gap-2 mr-4 items-center'>
+        {!verifyToken() ? (
           <>
-            <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150 '> <Link to="/login">Login</Link></div>
-            <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150 '><Link to="/subscribe">Subscribe</Link></div>
+            <NavButton to="/login">Login</NavButton>
+            <NavButton to="/subscribe">Subscribe</NavButton>
           </>
-        }
-            
-            
-            {verifyToken() ?
-            <>
-              <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150 '><Link to="/files">My Files</Link> </div>
-              <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150 '> <Link to="/disconnect">Disconnect</Link> </div>
-              <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150 '><Link to="/buy">Buy More Space</Link></div>
-              <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150 '><Link to="/account">Account</Link></div>
-             
-             
-              
-              <>{console.log(checkAdmin())}</>
-
-              {isAdmin ?
-                <>
-                 <div className='p-2 rounded-full hover:underline hover:bg-zinc-200 hover:font-bold duration-150 '>
-                  <Link to="/admin">admin overview</Link>
-                 </div>
-                  
-                </>
-                :
-                <>
-                
-                </>
-              }
-
-              
-              
-            </>
-            :
-            ""
-          }
-            
-        </div>
-    </div>
-  )
+        ) : (
+          <>
+            <NavButton to="/files">My Files</NavButton>
+            <NavButton to="/account">Account</NavButton>
+            <NavButton to="/buy">Buy More Space</NavButton>
+            {isAdmin && <NavButton to="/admin">Dashboard Admin</NavButton>}
+            <NavButton to="/disconnect">Disconnect</NavButton>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 }
